4343417a374008e0974436fc1b9d5b34b004dcc2
infuriating <luca@infs.world>
2024-04-06 15:18:58 +0200

    Add games section to Home component

--- src/app/(pages)/(home)/components/Home.tsx
+++ src/app/(pages)/(home)/components/Home.tsx
@@ -10,9 +10,11 @@ import { buttonVariants } from "@/components/ui/button";
 export default function Home(params: {
   preloadedProjects: Preloaded<typeof api.project.getAll>;
   preloadedBlogPosts: Preloaded<typeof api.blog.getAll>;
+  preloadedGames: Preloaded<typeof api.game.getAll>;
 }) {
   const projects = usePreloadedQuery(params.preloadedProjects);
   const blogPosts = usePreloadedQuery(params.preloadedBlogPosts);
+  const games = usePreloadedQuery(params.preloadedGames);
 
   projects.sort((a, b) => b._creationTime - a._creationTime);
   blogPosts.sort((a, b) => b._creationTime - a._creationTime);
@@ -22,7 +24,7 @@ export default function Home(params: {
       <div className="text-3xl lg:text-5xl bg-clip-text text-transparent font-bold bg-gradient-to-r w-max from-cyan-500 to-fuchsia-500">
         infs.world
       </div>
-      <div className="grid lg:grid-cols-2 gap-6 py-4 lg:py-12">
+      <div className="grid lg:grid-cols-3 gap-6 py-4 lg:py-12">
         <div>
           <div className="w-max py-2 space-y-1.5">
             <h2 className="text-4xl font-bold">Projects</h2>
@@ -71,6 +73,32 @@ export default function Home(params: {
             {blogPosts.map((blogPost) => (
               <Link href={`/blog-post/${blogPost.slug}`} key={blogPost._id}>
                 <li>{blogPost.title}</li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <div className="w-max py-2 space-y-1.5">
+            <h2 className="text-4xl font-bold">Games</h2>
+            <p className="text-sm">
+              There {games.length === 1 ? "is" : "are"} {games.length} game
+              {games.length === 1 ? "" : "s"}.
+            </p>
+            <Link
+              href={"/add-game"}
+              className={buttonVariants({
+                variant: "link",
+                className: "px-0",
+              })}
+            >
+              &rarr; Add Game
+            </Link>
+            <Separator className="bg-neutral-100/35" />
+          </div>
+          <ul>
+            {games.map((game) => (
+              <Link href={`/game/${game._id}`} key={game._id}>
+                <li>{game.title}</li>
               </Link>
             ))}
           </ul>
--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -5,11 +5,13 @@ import { api } from "../../../../convex/_generated/api";
 export default async function WorkWrapper() {
   const preloadedProjects = await preloadQuery(api.project.getAll);
   const preloadedBlogPosts = await preloadQuery(api.blog.getAll);
+  const preloadedGames = await preloadQuery(api.game.getAll);
 
   return (
     <Home
       preloadedProjects={preloadedProjects}
       preloadedBlogPosts={preloadedBlogPosts}
+      preloadedGames={preloadedGames}
     />
   );
 }
